486a3772aa242101e1607f1eeb7515a3640ad86f
anusha <anushanu454522@gamil.com>
2024-08-31 10:19:07 +0530

    java file

---
+++ add1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class add1
+{
+public static void main(String[] args)
+{
+Scanner sc=new scanner(system.in);
+int a=5,b=10
+c=a+b;
+system.out.println("sum of two numbers:"+c);
+int a=5,b=10
+c=a-b;
+system.out.println("subtract of two numbers:"+c);
+int a=5,b=10
+c=a*b;
+system.out.println("multiple of two numbers:"+c);
+int a=5,b=10
+c=a/b;
+system.out.println("dividing of two numbers:"+c);
+}
+}
